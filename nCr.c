8fbc71b93c1ebde5cc747171a126723c7f72e1f0
ishan7540 <50471705+ishan7540@users.noreply.github.com>
2023-11-04 17:47:09 +0530

    Create nCr.c

---
+++ nCr.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+int factorial(int x){
+    int fact =1;
+    for( int i=1;i<=x;i++){
+        fact = fact*i;
+    }
+    
+    return fact;
+}
+int main()
+ {
+    int n,r;
+    printf("Welcome to nCr calculator!!!\n");
+    printf("Enter the value to n and r:");
+    scanf("%d %d", &n,&r);
+    
+    int nfact = factorial(n);
+    int rfact = factorial(r);
+    int nrfact = factorial(n-r);
+    int nCr = nfact/(rfact*nrfact);
+    printf("%d",nCr);
+ }
